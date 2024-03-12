f9d3fa689fcae3fe49cab203d17b14b249b5fcd6
puvakovah <puvakova.h@gmail.com>
2024-03-12 18:13:13 +0100

    uprava stlpcov

--- zadanie01.c
+++ zadanie01.c
@@ -1,21 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
 #include<time.h>
 #define SIZE 9
+#define OK 0
+#define FAIL 1
 
 void vypis_sudoku(int a[SIZE][SIZE])
 {
 
         for (int row = 1; row <= SIZE; row++) {
         for (int col = 1; col <= SIZE; col++) {
-            printf("%d ", a[row][col]);
+          if(col%3==1)
+          {
+               printf("\t%d ", a[row][col]);
+          }
+          else
+          {
+               printf("1 ", a[row][col]);
+          }
+
         }
         printf("\n");
     } 
 }
 
-int main()
+
+void main(void)
 {
      int sudoku[SIZE][SIZE];
      vypis_sudoku(sudoku);
