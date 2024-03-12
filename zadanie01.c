4045d931944af6cca6596ff73df1382764cb564c
puvakovah <puvakova.h@gmail.com>
2024-03-12 19:20:09 +0100

    pridanie procedury generuj_sudoku

--- zadanie01.c
+++ zadanie01.c
@@ -32,20 +32,49 @@ void vypis_sudoku(int *sudoku)
     } 
 }
 
-int pozicie(int *sudoku, int row, int col, int num)
+int pozicie(int *sudoku, int row, int col, int num)//funkcia na kontrolu, ci moze byt cislo vlozene na danu poziciu
 {
      int s[SIZE][SIZE];
 
      s[SIZE][SIZE]=*sudoku;
-     for(int i=0; i<SIZE; i++)//kontrola riadku a stlpca ci je tam este mozne vypisat cislo
+     for(int i=0; i<SIZE; i++)//kontrola riadku a stlpca
      {
           if(s[row][i] == num || s[i][col] == num)
           return 0;
      } 
+     for(int i=0; i<3; i++)//schema 3x3
+     {
+          for(int j=0; j<3; j++)
+          {
+               if(s[i][j] == num)
+               return 0;
+          }
+     }
 }
 
+void generuj_sudoku(int *sudoku, int row, int col)
+{
+     int s[SIZE][SIZE];
+     srand(time(NULL));
+
+     s[SIZE][SIZE]=*sudoku;
+     for(int num=1; num<=SIZE; num++)
+     {
+          if(pozicie(sudoku,row,col,num))
+          {
+              s[row][col]=num;
+              vypis_sudoku(sudoku);  
+          }     
+     }
+}
 void main(void)
 {
      int *sudoku;
-     vypis_sudoku(sudoku);
+     for(int row=1; row<=SIZE; row++)
+     {
+          for(int col=0; col<=SIZE; col++)
+          {
+               generuj_sudoku(sudoku,row,col);
+          }
+     }
 }
