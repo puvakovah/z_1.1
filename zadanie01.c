dd30bb8254ac0eab9836c6b9c147890a94164861
puvakovah <puvakova.h@gmail.com>
2024-03-12 18:52:45 +0100

    kontrola riadku a stplca

--- zadanie01.c
+++ zadanie01.c
@@ -7,9 +7,11 @@
 
 void vypis_sudoku(int a[SIZE][SIZE])
 {
+     srand(time(NULL));
 
-        for (int row = 1; row <= SIZE; row++) {
-        for (int col = 1; col <= SIZE; col++) {
+     for (int row = 1; row <= SIZE; row++) {
+     for (int col = 1; col <= SIZE; col++) {
+          a[row][col]=rand()%9+1;//zatial iba pomocne vygenerovanie cisel do sudoku
           if(col%3==0)
           {
                printf("%d ", a[row][col]);
@@ -19,15 +21,28 @@ void vypis_sudoku(int a[SIZE][SIZE])
           {
                printf("%d ", a[row][col]);
           }
-
         }
-        if(row%3==0)
+     if(row%3==0)
           printf("\n\n");
-        else
+     else
           printf("\n");
     } 
 }
 
+int pozicie(int *sudoku, int row, int col, int num)
+{
+     int s[SIZE][SIZE];
+
+     for(int i=0; i<SIZE; i++)
+     {
+          s[i][i]= *sudoku;
+     }
+     for(int i=0; i<SIZE; i++)//kontrola riadku a stlpca ci je tam este mozne vypisat cislo
+     {
+          if(s[row][i] == num || s[i][col] == num)
+          return 0;
+     } 
+}
 
 void main(void)
 {
